bf155df71034e9cd8f0beb2dabc378aa88d9575e
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-06 12:23:52 -0300

    :lipstick: Estiilo global e fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const EstiloGLobal = createGlobalStyle`
+
+  html{
+  scroll-behavior: smooth;
+  }
+
+  @font-face {
+    font-family: "Lato";
+    src: url("./fonts/Lato/Lato-Light.ttf") format('truetype');
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: "Cinzel";
+    src: url('./fonts/Cinzel/Cinzel-Regular.ttf') format('truetype');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url('./fonts/Montserrat/Montserrat-VariableFont_wght.ttf') format('truetype');
+    font-weight: 100 300;
+  }
+
+  *{
+    padding:0;
+    margin:0;
+    font-size: 16px;
+    box-sizing: border-box;
+    list-style: none;
+    text-transform: none;
+    font-style: normal;
+    text-decoration: none;
+    font-family: "Montserrat";
+  }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <EstiloGLobal />
     <App />
   </React.StrictMode>
 );
