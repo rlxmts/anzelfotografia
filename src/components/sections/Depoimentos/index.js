210a1844a593a6df78d7eeb3e0efc7923593d279
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-07 18:17:24 -0300

    :construction: Construindo seção Depoimentos

--- src/components/sections/Depoimentos/index.js
+++ src/components/sections/Depoimentos/index.js
@@ -9,16 +9,39 @@ const SecaoDepoimentos = styled.section`
     }
 `
 
+const depoimentos = [
+    {
+        nome: 'Letícia',
+        foto: './img/leticia.jpg',
+        texto: 'Melhor fotógrafa do planeta! Já fiz 2 ensaios com ela e pretendo fazer sempre que der.'
+    },
+    {
+        nome: 'Thainá',
+        foto: './img/thaina.jpg',
+        texto: 'A Alessandra arrebenta demais! Tivemos um dia incrivel com um ensaio super leve e discontraído.'
+    },
+    {
+        nome: 'Marcelle',
+        foto: './img/marcele.jpg',
+        texto: 'Melhor fotógrafa do planeta! Já fiz 2 ensaios com ela e pretendo fazer sempre que der.'
+    }
+]
+
+
 const Depoimentos = () => {
     return(
         <SecaoDepoimentos>
             <Container className="depoimentos-container">
-                <Titulo titulo='Depoimentos' />
-                <div>
-                    {/* <img /> */}
-                    <span></span>
-                    <p></p>
-                </div>
+                <Titulo titulo='Depoimentos' img='./img/avaliation.svg'/>
+                {depoimentos.map( item => {
+                    return(
+                        <div key={item.nome}>
+                            <img src={item.foto} alt={item.nome} />
+                            <span>{item.nome}</span>
+                            <p>{item.texto}</p>
+                        </div>
+                    )
+                })}
             </Container>
         </SecaoDepoimentos>
     )
