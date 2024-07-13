f197a9dead3a2108d552b42682fab80e214d35c6
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-13 11:05:42 -0300

    :sparkles: Criando primeira Rota com React-router-dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createGlobalStyle } from 'styled-components';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -81,7 +82,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <EstiloGLobal />
-    <App />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
