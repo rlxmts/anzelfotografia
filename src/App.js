ba8d6cf86090b147c81222f1e050ff12fd240174
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-15 12:44:27 -0300

    :boom: Revertendo animações do AOS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import Banner from "./components/sections/Banner";
 import Portfolio from "./components/sections/Portfolio";
 import Sobre from "./components/sections/Sobre";
 import Depoimentos from "./components/sections/Depoimentos";
-import Aos from "aos";
-import 'aos/dist/aos.css';
-import { useEffect } from "react";
 
 const AppContainer = styled.div`
 
 `
 function App() {
-
-  useEffect( ()=>{
-    Aos.init({
-        duration:1000,
-        once: true
-    })
-},[])
 
   return (
     <AppContainer>
